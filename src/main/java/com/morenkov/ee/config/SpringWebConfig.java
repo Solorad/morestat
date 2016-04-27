bf610e2edbac500c4f6e6c783ce87b0aa328ce78
emorenkov <emorenkov@luxoft.com>
2016-04-27 18:52:44 +0300

    Update Use command clean spring-boot:run

--- src/main/java/com/morenkov/ee/config/SpringWebConfig.java
+++ src/main/java/com/morenkov/ee/config/SpringWebConfig.java
@@ -5,16 +5,12 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Equivalent for spring-web-config.xml
@@ -25,22 +21,6 @@ import org.springframework.web.servlet.view.JstlView;
 @ImportResource("/spring/controllers.xml")
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
     private static final Logger LOG = LogManager.getLogger(SpringWebConfig.class);
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
-    }
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/views/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
-    }
-
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
