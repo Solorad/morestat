6887080eaa748d383a13f3865e88428aecf8f5bd
emorenkov <emorenkov@luxoft.com>
2016-04-29 18:39:39 +0300

    Sync commit. Revert deleted classes.

---
+++ src/main/java/com/morenkov/ee/morestat/web/MainPageController.java
@@ -0,0 +1,154 @@
+package com.morenkov.ee.morestat.web;
+
+import com.morenkov.ee.morestat.utils.constants.Constants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.oauth.InstagramService;
+import org.jinstagram.entity.users.basicinfo.UserInfoData;
+import org.jinstagram.entity.users.feed.MediaFeed;
+import org.jinstagram.entity.users.feed.MediaFeedData;
+import org.jinstagram.exceptions.InstagramException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author emorenkov
+ */
+@Controller
+@SessionAttributes({"instagram", "instagramService"})
+public class MainPageController {
+    private static final Logger logger = LogManager.getLogger(MainPageController.class);
+//    private OAuth2RestTemplate oAuth2RestTemplate;
+
+    public static final String ACTIVE_TAB = "activeTab";
+
+    private InstagramService instagramService;
+    private Instagram instagram;
+
+    @Autowired
+    public MainPageController(InstagramService instagramService, Instagram instagram) {
+        this.instagramService = instagramService;
+        this.instagram = instagram;
+    }
+
+    @ModelAttribute
+    public void instagramAttribute(Model model) {
+        if (instagram != null && instagram.getAccessToken() != null) {
+            model.addAttribute(Constants.INSTAGRAM, instagram);
+        }
+    }
+
+    @ModelAttribute("instagramService")
+    public InstagramService instagramServiceAttribute() {
+        return instagramService;
+    }
+
+
+
+
+    @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
+    public String index(Model model) {
+        logger.debug("index() is executed!");
+        if (isAllowedResource()) {
+            return "profile";
+        }
+
+        model.addAttribute("authorizationUrl", instagramService.getAuthorizationUrl(null));
+        return "index";
+    }
+
+    @RequestMapping(value={"/profile"})
+    public String profile(Model model) {
+        if (!isAllowedResource()) {
+            return "index";
+        }
+        model.addAttribute(ACTIVE_TAB, "profile");
+
+        try {
+            UserInfoData userInfoData = instagram.getCurrentUserInfo().getData();
+            model.addAttribute("userInfoData", userInfoData);
+            MediaFeed recentMediaFeed = instagram.getRecentMediaFeed(userInfoData.getId());
+
+            List<MediaFeedData> mediaFeed = recentMediaFeed.getData();
+            int totalLikes = mediaFeed.stream().collect(
+                    Collectors.summingInt((MediaFeedData data) -> data.getLikes().getCount()));
+            int totalComments = mediaFeed.stream().collect(
+                    Collectors.summingInt((MediaFeedData data) -> data.getComments().getCount()));
+
+            List<MediaFeedData> sortedByLikesMedia = new ArrayList<>(mediaFeed);
+            sortedByLikesMedia.sort(
+                    (MediaFeedData data1, MediaFeedData data2)
+                            -> data2.getLikes().getCount() - data1.getLikes().getCount());
+            List<MediaFeedData> sortedByCommentsMedia = new ArrayList<>(mediaFeed);
+            sortedByCommentsMedia.sort(
+                    (MediaFeedData data1, MediaFeedData data2)
+                            -> data2.getComments().getCount() - data1.getComments().getCount());
+
+            model.addAttribute("totalLikes", totalLikes);
+            model.addAttribute("totalComments", totalComments);
+            model.addAttribute("sortedByLikesMedia", sortedByLikesMedia);
+            model.addAttribute("sortedByCommentsMedia", sortedByCommentsMedia);
+
+        } catch (InstagramException e) {
+            logger.error("Instagram exception occurred.");
+        }
+        return "profile";
+    }
+
+
+
+
+    @RequestMapping(value={"/gallery"})
+    public String gallery(Model model) {
+        logger.debug("gallery() is executed!");
+        if (!isAllowedResource()) {
+            return "index";
+        }
+
+        model.addAttribute(ACTIVE_TAB, "gallery");
+        return "gallery";
+    }
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpSession session, Model model) {
+        logger.debug("logout() is executed!");
+        instagram.setAccessToken(null);
+        model.addAttribute(ACTIVE_TAB, "logout");
+        return "logout";
+    }
+
+    @RequestMapping(value="/popular")
+    public String popular(Model model) {
+        logger.debug("popular() is executed!");
+        if (!isAllowedResource()) {
+            return "index";
+        }
+        model.addAttribute(ACTIVE_TAB, "popular");
+        return "popular";
+    }
+
+    @RequestMapping(value="/search")
+    public String search(Model model) {
+        logger.debug("search() is executed!");
+        if (!isAllowedResource()) {
+            return "index";
+        }
+        model.addAttribute(ACTIVE_TAB, "search");
+        return "search";
+    }
+
+    private boolean isAllowedResource() {
+        return instagram.getAccessToken() != null;
+    }
+}
---
+++ src/main/java/com/morenkov/ee/morestat/web/SecurityController.java
@@ -0,0 +1,43 @@
+package com.morenkov.ee.morestat.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.model.Token;
+import org.jinstagram.auth.model.Verifier;
+import org.jinstagram.auth.oauth.InstagramService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
+/**
+ * @author emorenkov
+ */
+@Controller
+public class SecurityController {
+    private static final Logger LOG = LogManager.getLogger(MainPageController.class);
+    private final Instagram instagram;
+    private final InstagramService instagramService;
+
+    @Autowired
+    public SecurityController(Instagram instagram, InstagramService instagramService) {
+        this.instagram = instagram;
+        this.instagramService = instagramService;
+    }
+
+
+    @RequestMapping(value = {"/login/oauth"})
+    public String authorize(@RequestParam(value = "code", required = false) String code) {
+        if (!isEmpty(code)) {
+            Verifier verifier = new Verifier(code);
+
+            Token accessToken = instagramService.getAccessToken(null, verifier);
+            instagram.setAccessToken(accessToken);
+            return "redirect:/profile";
+        }
+        return "redirect:/";
+    }
+}
